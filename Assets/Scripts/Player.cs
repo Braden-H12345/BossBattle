b989f7e83183533c459376868911eb9f49d07c35
BradenH <bradenhonea@yahoo.com>
2021-09-13 14:37:54 -0500

    Imported all assets from TankController
    Imported assets to use for this project if needed. Removed treasure related things though as it is not likely to be used for this project. May add back later but I do not see a need for it in the scope of a boss battle...

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+[RequireComponent(typeof(TankController))]
+public class Player : MonoBehaviour
+{
+    [SerializeField] Text healthText;
+    //[SerializeField] Text treasureText;
+    [SerializeField] int _maxHealth = 3;
+    private int treasureCount = 0;
+    int _currentHealth;
+    private bool isInvincible;
+    
+
+    public void setInvincible(bool b)
+    {
+        isInvincible = b;
+    }
+
+    TankController _tankController;
+    private void Awake()
+    {
+        _tankController = GetComponent<TankController>();
+        isInvincible = false;
+    }
+
+    private void Start()
+    {
+        _currentHealth = _maxHealth;
+    }
+
+    public void IncreaseTreasure()
+    {
+        //treasureCount += 1;
+        //treasureText.text = "Treasure: " + treasureCount;
+    }
+    public void IncreaseHealth(int amount)
+    {
+        _currentHealth = Mathf.Clamp(_currentHealth, 0, _maxHealth);
+        _currentHealth += amount;
+        healthText.text = "Health: " + _currentHealth;
+    }
+
+    public void DecreaseHealth(int amount)
+    {
+        if (isInvincible == false)
+        {
+            _currentHealth -= amount;
+            healthText.text = "Health: " + _currentHealth;
+
+            if (_currentHealth <= 0)
+            {
+                Kill();
+            }
+        }
+    }
+
+    public void Kill()
+    {
+        _currentHealth = 0;
+        healthText.text = "Health: " + _currentHealth;
+        gameObject.SetActive(false);
+        //play partciles & sounds
+    }
+}
---
+++ Assets/Scripts/PowerUpBase.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class PowerUpBase : MonoBehaviour
+{
+    protected abstract void PowerUp(Player player);
+    protected abstract void PowerDown(Player player);
+
+    [SerializeField] float powerupDuration = 5.0f;
+    [SerializeField] Transform visualsToDisable;
+    [SerializeField] ParticleSystem _collectParticles;
+    [SerializeField] AudioClip _collectSound;
+
+
+    private Player forPowerDown;
+    private float timeElapsed = 0f;
+    private bool entered = false;
+
+
+    Rigidbody _rb;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Player player = other.gameObject.GetComponent<Player>();
+        if (player != null)
+        {
+            Feedback();
+            forPowerDown = player;
+            entered = true;
+            visualsToDisable.gameObject.SetActive(false);
+            BoxCollider c = this.gameObject.GetComponent<BoxCollider>();
+            c.enabled = false;
+            PowerUp(player);
+        }
+    }
+
+    private void Feedback()
+    {
+        if (_collectParticles != null)
+        {
+            _collectParticles = Instantiate(_collectParticles, transform.position, Quaternion.identity);
+        }
+
+        if (_collectSound != null)
+        {
+            AudioHelper.PlayClip2D(_collectSound, 1f);
+        }
+    }
+
+    protected virtual void Movement(Rigidbody rb)
+    {
+        Quaternion turnOffset = Quaternion.Euler(-1, 0, 1);
+        rb.MoveRotation(_rb.rotation * turnOffset);
+    }
+
+
+    private void Update()
+    {
+        if(entered == true)
+        {
+            
+            timeElapsed += Time.deltaTime;
+            Debug.Log(timeElapsed);
+            if (timeElapsed >= powerupDuration)
+            {
+                entered = false;
+                PowerDown(forPowerDown);
+                Destroy(this.gameObject);
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Movement(_rb);
+    }
+}

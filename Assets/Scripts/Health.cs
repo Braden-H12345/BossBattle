ceec46284224c319ee8f32fc3e9276eb31ec009a
BradenH <bradenhonea@yahoo.com>
2021-09-16 11:39:33 -0500

    Revert "Removed scripts that I was not using"
    This reverts commit 6e0dac6389cf242ca76400d77c3619e9e231b2f4.

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour, IDamageable
+{
+    [SerializeField] int _maxHealth;
+    [SerializeField] ParticleSystem _damageParticles;
+    [SerializeField] AudioClip _damageSound;
+    [SerializeField] ParticleSystem _killParticles;
+    [SerializeField] AudioClip _killSound;
+
+
+    private int _currentHealth;
+
+    void Awake()
+    {
+        _currentHealth = _maxHealth;
+    }
+
+    public void takeDamage(int damage)
+    {
+        _currentHealth -= damage;
+        Feedback();
+        if (_currentHealth <= 0)
+        {
+            KillFeedback();
+            Kill();
+        }
+    }
+
+    void KillFeedback()
+    {
+        if (_killParticles != null)
+        {
+            _killParticles = Instantiate(_killParticles, this.transform.position, Quaternion.identity);
+            _killParticles.Play();
+
+        }
+
+        if (_killSound != null)
+        {
+            AudioHelper.PlayClip2D(_killSound, 1f);
+        }
+    }
+
+    public void Kill()
+    {
+        gameObject.SetActive(false);
+    }
+
+    void Feedback()
+    {
+        if (_damageParticles != null)
+        {
+            _damageParticles = Instantiate(_damageParticles, this.transform.position, Quaternion.identity);
+            _damageParticles.Play();
+
+        }
+
+        if (_damageSound != null)
+        {
+            AudioHelper.PlayClip2D(_damageSound, 1f);
+        }
+    }
+}
---
+++ Assets/Scripts/Treasure.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Treasure : CollectableBase
+{
+    // Start is called before the first frame update
+
+
+    protected override void Collect(Player player)
+    {
+        //player.IncreaseTreasure();
+    }
+
+    protected override void Movement(Rigidbody rb)
+    {
+        Quaternion turnOffset = Quaternion.Euler
+            ( MovementSpeed, -MovementSpeed, MovementSpeed);
+        rb.MoveRotation(rb.rotation * turnOffset);
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

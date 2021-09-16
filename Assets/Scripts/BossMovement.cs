25ca66604db8c02f86d20d9612860871f1c0a234
BradenH <bradenhonea@yahoo.com>
2021-09-15 20:23:17 -0500

    Dash movement works
    Dash works... will have to smooth it out over the next couple weeks but functionality is there

--- Assets/Scripts/BossMovement.cs
+++ Assets/Scripts/BossMovement.cs
@@ -6,6 +6,9 @@ public class BossMovement : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField] Player player;
+
+    private bool isDashing = false;
+    private float timeElapsed = 0f;
     void Start()
     {
         
@@ -15,6 +18,27 @@ public class BossMovement : MonoBehaviour
     void Update()
     {
         transform.LookAt(player.transform);
-        transform.position = Vector3.MoveTowards(transform.position, player.transform.position, .008f);
+        transform.position = Vector3.MoveTowards(transform.position, player.transform.position, .016f);
+
+        if (!isDashing && timeElapsed >= 6)
+        {
+            StartCoroutine(Dash());
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        timeElapsed += Time.deltaTime;
+    }
+
+    IEnumerator Dash()
+    {
+        isDashing = true;
+        for(int i=0; i<9;i++)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, .9f);
+        }
+        yield return new WaitForSeconds(12f);
+        isDashing = false;
     }
 }
